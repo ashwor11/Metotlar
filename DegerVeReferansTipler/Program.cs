33bdf68c300d0c48bca968a44a2caaa8fecc3985
Kerim Hasan YILDIRIM <kerimhassan81@gmail.com>
2021-01-23 00:12:16 +0300

    22 ocak ders sonu

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -10,9 +10,9 @@ namespace DegerVeReferansTipler
 
             int sayi1 = 10;
             int sayi2 = 30;
-            sayi1 = sayi1;
+            sayi1 = sayi2;
             sayi2 = 65;
-
+            
             int[] sayilar1 = new int[] { 10, 20, 30 };
             int[] sayilar2 = new int[] { 100, 200, 300 };
             sayilar1 = sayilar2;
